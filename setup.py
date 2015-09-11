65bd5cb79e2083ca43ae1d223e1d301ee7b7c5e5
Fatih Erikli <fatiherikli@gmail.com>
2015-09-12 01:09:42 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='graphcommons',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/graphcommons/graphcommons-python',
+    license='MIT',
+    author='fatiherikli',
+    author_email='fatiherikli@gmail.com',
+    description='Python Wrapper For Graph Commons API.  '
+)
